c94dfd4d1d1201fdd34df4a86338d2d9bf6252eb
qingyuan <qingyuan@ftsafe.com>
2017-04-28 16:56:56 +0800

    添加W2加解密接口

---
+++ code/wp-virtual-hsm/src/main/java/org/warmsheep/encoder/actor/processor/W2Processor.java
@@ -0,0 +1,66 @@
+package org.warmsheep.encoder.actor.processor;
+
+import java.io.Serializable;
+
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOUtil;
+import org.jpos.transaction.Context;
+import org.warmsheep.encoder.actor.AbsActor;
+import org.warmsheep.encoder.bean.W2CommandBean;
+import org.warmsheep.encoder.constants.RespCmdType;
+import org.warmsheep.encoder.ic.RespCodeIC;
+import org.warmsheep.encoder.ic.TxnIC;
+import org.warmsheep.encoder.security.des.impl.DESede;
+
+public class W2Processor extends AbsActor {
+
+	@Override
+	public int prepare(long id, Serializable serializable) {
+        Context context = (Context) serializable;
+        try {
+            ISOMsg reqMsg = (ISOMsg) context.get(TxnIC.MSG_HSM);
+            String header = reqMsg.getString(0);
+            String commandType = reqMsg.getString(1);
+            String requestData = reqMsg.getString(2);
+
+            W2CommandBean commandBean = W2CommandBean.build(header, commandType, requestData);
+            String encMode = commandBean.getEncModeFlag();
+            String solutionId = commandBean.getSolutionId();
+            String rootKey = commandBean.getRootKey().substring(1);
+            String data = commandBean.getData();
+            
+            context.put(TxnIC.RESULT_TYPE, RespCmdType.W3);
+            String result = "";
+            if (encMode.equals("1")){ // 3DES-CBC
+                
+            } else if(encMode.equals("2")) { // DES-CBC
+            	
+            } else if (encMode.equals("3")) { // DES-ECB
+            	
+            } else if (encMode.equals("4")) { // 3DES-ECB
+            	DESede desEde = DESede.newInstance16(ISOUtil.hex2byte(rootKey));
+            	if(solutionId.equals("0")) // 加密
+            		result = ISOUtil.hexString(desEde.encrypt(ISOUtil.hex2byte(data)));
+            	else if (solutionId.equals("1")) // 解密
+            		result = ISOUtil.hexString(desEde.decrypt(ISOUtil.hex2byte(data)));
+            	else {
+            		context.put(TxnIC.RESULT_CODE, RespCodeIC.UNDEFINED_SOLUTION_ID);
+            	}
+            	
+            } else if (encMode.equals("0")) {
+            	
+            } else {
+                context.put(TxnIC.RESULT_CODE, RespCodeIC.MODE_ERROR);
+            }
+            String resultLen = String.format("%04d", result.length()/2);
+            context.put(TxnIC.RESULT_DATA, resultLen+result);
+            return PREPARED | NO_JOIN;
+        } catch (Exception e){
+            logger.error("W2指令处理出现异常", e);
+            context.put(TxnIC.RESULT_CODE, RespCodeIC.OTHER_ERROR);
+            return ABORTED | NO_JOIN;
+        }
+
+        }
+
+}
---
+++ code/wp-virtual-hsm/src/main/java/org/warmsheep/encoder/bean/W2CommandBean.java
@@ -0,0 +1,169 @@
+package org.warmsheep.encoder.bean;
+
+public class W2CommandBean extends CommandBean {
+	
+	private static final long serialVersionUID = 657234656124409629L;
+
+	private W2CommandBean(){}
+	
+	public static W2CommandBean build(String header,String commandType,String commandContent) {
+		W2CommandBean commandBean = new W2CommandBean();
+		
+		commandBean.setCommandHeader(header);
+        commandBean.setCommandType(commandType);
+        
+        int index=0;
+        
+        commandBean.setEncModeFlag(commandContent.substring(index, index+=1));
+        commandBean.setSolutionId(commandContent.substring(index, index+=1));
+        commandBean.setRootKeyType(commandContent.substring(index, index+=3));
+        commandBean.setRootKey(commandContent.substring(index, index+=33));
+		int tmp = Integer.valueOf(commandContent.substring(index, index+=1));
+		commandBean.setDiversifyNum(tmp);
+        commandBean.setDiversifyData(commandContent.substring(index, index+=tmp));
+        commandBean.setSessionKeyFlag(commandContent.substring(index, index+=1));
+        if (!commandBean.getSessionKeyFlag().equals("0"))
+        	commandBean.setSessionKey(commandContent.substring(index, index+=32));
+        commandBean.setDataPaddingFlag(commandContent.substring(index, index+=1));
+        tmp = Integer.valueOf(commandContent.substring(index, index+=4));
+        commandBean.setDataLen(tmp);
+        commandBean.setData(commandContent.substring(index, index+=tmp*2));
+        return commandBean;
+	}
+	/**
+	 * 加密模式标识
+	 */
+	private String encModeFlag;
+	/**
+	 * 方案ID
+	 */
+	private String solutionId;
+	/**
+	 * 根秘钥类型
+	 */
+	private String rootKeyType;
+	/**
+	 * 根秘钥
+	 */
+	private String rootKey;
+	/**
+	 * 离散次数
+	 */
+	private int diversifyNum;
+	/**
+	 * 离散数据
+	 */
+	private String diversifyData;
+	/**
+	 * 过程密钥标识
+	 */
+	private String sessionKeyFlag;
+	/**
+	 * 过程密钥
+	 */
+	private String sessionKey;
+	/**
+	 * 数据填充标识
+	 */
+	private String dataPaddingFlag;
+	/**
+	 * 数据长度
+	 */
+	private int dataLen;
+	/**
+	 * 数据
+	 */
+	private String data;
+	
+	public String getEncModeFlag() {
+		return encModeFlag;
+	}
+
+	public void setEncModeFlag(String encModeFlag) {
+		this.encModeFlag = encModeFlag;
+	}
+
+	public String getSolutionId() {
+		return solutionId;
+	}
+
+	public void setSolutionId(String solutionId) {
+		this.solutionId = solutionId;
+	}
+
+	public String getRootKeyType() {
+		return rootKeyType;
+	}
+
+	public void setRootKeyType(String rootKeyType) {
+		this.rootKeyType = rootKeyType;
+	}
+
+	public String getRootKey() {
+		return rootKey;
+	}
+
+	public void setRootKey(String rootKey) {
+		this.rootKey = rootKey;
+	}
+
+	public int getDiversifyNum() {
+		return diversifyNum;
+	}
+
+	public void setDiversifyNum(int diversifyNum) {
+		this.diversifyNum = diversifyNum;
+	}
+
+	public String getDiversifyData() {
+		return diversifyData;
+	}
+
+	public void setDiversifyData(String diversifyData) {
+		this.diversifyData = diversifyData;
+	}
+
+	public String getSessionKeyFlag() {
+		return sessionKeyFlag;
+	}
+
+	public void setSessionKeyFlag(String sessionKeyFlag) {
+		this.sessionKeyFlag = sessionKeyFlag;
+	}
+
+	public String getSessionKey() {
+		return sessionKey;
+	}
+
+	public void setSessionKey(String sessionKey) {
+		this.sessionKey = sessionKey;
+	}
+
+	public String getDataPaddingFlag() {
+		return dataPaddingFlag;
+	}
+
+	public void setDataPaddingFlag(String dataPaddingFlag) {
+		this.dataPaddingFlag = dataPaddingFlag;
+	}
+
+	public int getDataLen() {
+		return dataLen;
+	}
+
+	public void setDataLen(int dataLen) {
+		this.dataLen = dataLen;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
